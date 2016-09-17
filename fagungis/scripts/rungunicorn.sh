d4ff08db372e85f7c2cafa6603d7c8004efe1252
DILIP S <dilipskumar1410@gmail.com>
2016-09-17 23:43:29 +0530

    first commit

---
+++ fagungis/scripts/rungunicorn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+# go in your project root
+cd %(django_project_root)s
+# set PYTHONPATH to cwd
+export PYTHONPATH=`pwd`
+# activate the virtualenv
+source %(virtenv)s/bin/activate
+# start gunicorn with all options earlier declared in fabfile.py
+exec gunicorn lytyfy_rest.wsgi:application -w %(gunicorn_workers)s \
+    --user=%(django_user)s --group=%(django_user_group)s \
+    --bind=%(gunicorn_bind)s --log-level=%(gunicorn_loglevel)s \
+    --log-file=%(gunicorn_logfile)s 2>>%(gunicorn_logfile)s
